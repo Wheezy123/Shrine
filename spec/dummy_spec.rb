98d34307acbb3f5bee70d56300070b5362010129
Wheezy123 <johnsonjeromec@gmail.com>
2016-05-01 22:56:25 -0500

    add rspec, beginning to write tests

---
+++ spec/dummy_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper.rb"
+
+describe "testing that rspec is configrued" do
+
+  it "should pass" do
+    expect(true).to eq(true)
+  end
+
+  # it "can fail" do
+  #   expect(false).to eq(true)
+  # end
+end
+
